87b26dc2fddd9597ac6b2089ec3213f05329ee35
Alberto Guerra Gonzalez <aguerra@riotgames.com>
2021-04-19 01:49:20 +0100

    Fix build

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,35 +1,37 @@
 const siteMetadata = require('./site-metadata.json')
 
+const plugins = [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-source-data`,
+    `gatsby-transformer-remark`,
+    {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+            name: `pages`,
+            path: `${__dirname}/src/pages`
+        }
+    },
+    {
+        resolve: `gatsby-plugin-sass`,
+        options: {}
+    },
+    {
+        resolve: `gatsby-remark-page-creator`,
+        options: {}
+    },
+    {
+        resolve: `@stackbit/gatsby-plugin-menus`,
+        options: {
+            sourceUrlPath: `fields.url`,
+            pageContextProperty: `menus`,
+        }
+    }
+];
+
 module.exports = {
     pathPrefix: '/',
     siteMetadata: siteMetadata,
-    plugins: [
-        `gatsby-plugin-react-helmet`,
-        `gatsby-source-data`,
-        `gatsby-transformer-remark`,
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `pages`,
-                path: `${__dirname}/src/pages`
-            }
-        },
-        {
-            resolve: `gatsby-plugin-sass`,
-            options: {}
-        },
-        {
-            resolve: `gatsby-remark-page-creator`,
-            options: {}
-        },
-        {
-            resolve: `@stackbit/gatsby-plugin-menus`,
-            options: {
-                sourceUrlPath: `fields.url`,
-                pageContextProperty: `menus`,
-            }
-        }
-    ]
+    plugins: plugins
 };
 
 const TRACKING_ID = process.env.TRACKING_ID;
